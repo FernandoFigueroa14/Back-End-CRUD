8e65a3c8242ff9ea1697e8b47f3437941960509f
FernandoFigueroa14 <luisfersurfer14@hotmail.com>
2021-10-18 17:33:55 -0500

    DB conection with Sequelize and mysql2

---
+++ src/database/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "development": {
+    "username": "user5",
+    "password": "root",
+    "database": "playlist",
+    "host": "18.234.222.26",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "user5",
+    "password": "root",
+    "database": "playlist",
+    "host": "18.234.222.26",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "user5",
+    "password": "root",
+    "database": "playlist",
+    "host": "18.234.222.26",
+    "dialect": "mysql"
+  }
+}
